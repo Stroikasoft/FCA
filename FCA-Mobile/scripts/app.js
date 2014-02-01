292c0a98d88ae58ed798b1bd9e384834238a2946
Omar Lakem <omarlakem@hotmail.com>
2014-02-01 11:22:04 -0600

    Changes made by Omar Lakem (omarlakem@hotmail.com).

--- FCA-Mobile/scripts/app.js
+++ FCA-Mobile/scripts/app.js
@@ -1,24 +1,8 @@
-﻿(function (global) {
-	var mobileSkin = "",
-		app = global.app = global.app || {};
+(function (global) {
 
-	document.addEventListener('deviceready', function () {
-		navigator.splashscreen.hide();
-		$(document.body).height(window.innerHeight);
-	}, false);
+
 
 	app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout"});
 
-	app.changeSkin = function (e) {
-		if (e.sender.element.text() === "Flat") {
-			e.sender.element.text("Native");
-			mobileSkin = "flat";
-		}
-		else {
-			e.sender.element.text("Flat");
-			mobileSkin = "";
-		}
-
-		app.application.skin(mobileSkin);
-	};
+	
 })(window);
